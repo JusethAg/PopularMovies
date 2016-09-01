26dfdc7b445bfc0c46796052c370c0437fb2ce31
JusethAg <mauagueda@gmail.com>
2016-09-01 17:33:05 -0500

    Do view for main screen
    Layout for main activity (recycler view and card view design) is done.

--- app/src/main/java/com/jusethag/popularmovies/main/ui/MainActivity.java
+++ app/src/main/java/com/jusethag/popularmovies/main/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jusethag.popularmovies;
+package com.jusethag.popularmovies.main.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jusethag.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
